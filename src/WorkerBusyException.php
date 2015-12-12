86d2d7541d13645104205bf0eeae15113c48a329
C. Davison <connorpdavison@gmail.com>
2015-12-12 23:48:05 +0000

    WorkerBusyException now constructs itself based on the busy worker. The worker may be retrieved when this exception is caught.

--- src/WorkerBusyException.php
+++ src/WorkerBusyException.php
@@ -1,4 +1,52 @@
 <?php
 namespace CDavison\Queue;
 
-class WorkerBusyException extends \Exception {}
+use CDavison\Queue\WorkerInterface;
+
+class WorkerBusyException extends \Exception
+{
+    /**
+     * The busy worker.
+     *
+     * @var WorkerInterface
+     */
+    protected $worker;
+
+    /**
+     * Create a busy worker exception.
+     *
+     * @param WorkerInterface The busy worker.
+     * @param string|null $message
+     * @param integer $code
+     * @param \Exception|null $previous
+     */
+    public function __construct(
+        $worker,
+        $message = null,
+        $code = 0,
+        \Exception $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->setWorker($worker);
+    }
+
+    /**
+     * Attach a worker to this exception.
+     *
+     * @param WorkerInterface $worker
+     */
+    protected function setWorker(WorkerInterface $worker)
+    {
+        $this->worker = $worker;
+    }
+
+    /**
+     * Retrieve the worker that generated this error.
+     * 
+     * @return WorkerInterface
+     */
+    public function getWorker()
+    {
+        return $this->worker;
+    }
+}
