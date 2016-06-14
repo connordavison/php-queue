78da2128da0ca752c59c5271d57b7646c76c1f9c
C. Davison <connorpdavison@gmail.com>
2016-06-15 00:26:27 +0100

    Removed unnecessary methods from DispatcherInterface.

--- src/Dispatchers/DispatcherInterface.php
+++ src/Dispatchers/DispatcherInterface.php
@@ -13,28 +13,4 @@ interface DispatcherInterface
      * @return void
      */
     public function run();
-
-    /**
-     * Dispatch a job through this interface.
-     *
-     * @param mixed $payload
-     * @return void
-     */
-    public function dispatch($payload);
-
-    /**
-     * Set the queue on which this dispatcher should run.
-     *
-     * @param QueueInterface $queue
-     * @return void
-     */
-    public function setQueue(QueueInterface $queue);
-
-    /**
-     * Set the worker to which this dispatcher will dispatch jobs.
-     *
-     * @param WorkerInterface $worker
-     * @return void
-     */
-    public function setWorker(WorkerInterface $worker);
 }
